8ab3bad6502f5b95f395ca2d23057fada98375da
Nikita <nikitadmitrem@gmail.com>
2024-01-16 11:53:54 +0300

    Fixes to "RomanToInteger"
    Now it's my decision

--- src/13-RomanToInteger.js
+++ src/13-RomanToInteger.js
@@ -3,28 +3,32 @@ const romanToInt = str => {
    const symbols = ["I", "V", "X", "L", "C", "D", "M"]
 
    let value = 1
-
    for(let i = 0; i < symbols.length; i++) {
       map.set(symbols[i], value)
       i % 2 === 0 ? value *= 5 : value *= 2
    }
 
-   let result = 0;
+   let maxValue = 0
+   let result = 0
 
-   for(let i = 0; i < str.length; i++) {
-      const current = map.get(str[i])
-      const next = map.get(str[i + 1])
+   for(let i = str.length - 1; i >= 0; i--) {
+      const symbol = map.get(str[i])
 
-      current <= next ? result -= current : result += current
+      if (symbol >= maxValue) {
+         maxValue = symbol
+         result += symbol
+      } else if (symbol < maxValue) {
+         result -= symbol
+      }
    }
 
    return result
 }
 
-const s1 = "XIIIV"
+const s1 = "XVVV"
 const s2 = "LVIII"
 const s3 = "XIVII"
 
 console.log(romanToInt(s1))
-// console.log(romanToInt(s2))
-// console.log(romanToInt(s1))
+console.log(romanToInt(s2))
+console.log(romanToInt(s3))
