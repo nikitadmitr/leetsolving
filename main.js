66066506e40f969cefb445c663d6cc1a5facc003
Nikita <nikitadmitrem@gmail.com>
2024-01-19 12:33:11 +0300

    Added gitignore

--- main.js
+++
@@ -1,5 +0,0 @@
-// import './src/1-TwoSum.js'
-// import './src/9-PalindromeNumber.js'
-// import './src/13-RomanToInteger.js'
-// import './src/14-LongestCommonPrefix.js'
-import './src/2723-AddTwoPromises.js'
